efde683e1406c8b2e696e360c345f62603ac92d0
Ruben Marin <ams.rmarin@ext.aragon.es>
2024-04-29 15:54:47 +0200

    REMOVE LOGOUT REQUEST

--- src/main/java/es/aragon/example/springboot/saml2/SecurityConfig.java
+++ src/main/java/es/aragon/example/springboot/saml2/SecurityConfig.java
@@ -86,9 +86,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	 			.assertionConsumerServiceLocation(assertingConsumerServiceLocation)
 	            .assertingPartyDetails(party -> party
 	                .entityId("http://ssoa.aragon.es/adfs/services/trust")
-	                //.singleSignOnServiceLocation("https://ssoa.aragon.es/adfs/ls/IdpInitiatedSignon")
+	                .singleSignOnServiceLocation("https://ssoa.aragon.es/adfs/ls/IdpInitiatedSignon")
 	                //.singleLogoutServiceLocation("https://samladfs-tpvams.apps.pre.aragon.es/logout/saml2/slo")
-	                .singleSignOnServiceLocation("https://ssoa.aragon.es/adfs/ls/?wa=wsignout1.0")
+	                .singleLogoutServiceLocation("https://ssoa.aragon.es/adfs/ls/?wa=wsignout1.0")
 	                .wantAuthnRequestsSigned(false)
 	                .verificationX509Credentials(c -> c.add(credential))
 	            ).build();
