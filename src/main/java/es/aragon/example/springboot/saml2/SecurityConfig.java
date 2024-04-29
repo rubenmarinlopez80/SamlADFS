2b2b8b3e831e84bc33ac6828bf6a08fd33cd1f7a
Ruben Marin <ams.rmarin@ext.aragon.es>
2024-04-29 13:55:38 +0200

    REMOVE LOGOUT REQUEST

--- src/main/java/es/aragon/example/springboot/saml2/SecurityConfig.java
+++ src/main/java/es/aragon/example/springboot/saml2/SecurityConfig.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.opensaml.saml.saml2.core.LogoutRequest;
+import org.opensaml.saml.saml2.core.NameID;
 import org.opensaml.security.x509.X509Support;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -19,13 +21,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.saml2.core.Saml2X509Credential;
+import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticatedPrincipal;
 import org.springframework.security.saml2.provider.service.metadata.OpenSamlMetadataResolver;
 import org.springframework.security.saml2.provider.service.registration.InMemoryRelyingPartyRegistrationRepository;
 import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistration;
 import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrationRepository;
 import org.springframework.security.saml2.provider.service.servlet.filter.Saml2WebSsoAuthenticationFilter;
 import org.springframework.security.saml2.provider.service.web.DefaultRelyingPartyRegistrationResolver;
+import org.springframework.security.saml2.provider.service.web.RelyingPartyRegistrationResolver;
 import org.springframework.security.saml2.provider.service.web.Saml2MetadataFilter;
+import org.springframework.security.saml2.provider.service.web.authentication.logout.OpenSaml4LogoutRequestResolver;
 import org.springframework.security.saml2.provider.service.web.authentication.logout.Saml2LogoutRequestResolver;
 
 @Configuration
@@ -38,19 +43,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
+
 		http
 	    	.authorizeRequests(authorize -> 
 	    		authorize.antMatchers("/").permitAll().anyRequest().authenticated()
 	        )
 	    	.saml2Login(Customizer.withDefaults())
-			.saml2Logout(Customizer.withDefaults());
+	    	.saml2Logout(Customizer.withDefaults());
 
 
 		// add auto-generation of ServiceProvider Metadata
 		Converter<HttpServletRequest, RelyingPartyRegistration> relyingPartyRegistrationResolver = new DefaultRelyingPartyRegistrationResolver(relyingPartyRegistrationRepository);
 		Saml2MetadataFilter filter = new Saml2MetadataFilter(relyingPartyRegistrationResolver, new OpenSamlMetadataResolver());
 		http.addFilterBefore(filter, Saml2WebSsoAuthenticationFilter.class);
+		
 			
 	}
 
@@ -87,20 +93,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	            ).build();
 	    return new InMemoryRelyingPartyRegistrationRepository(registration);
 	}
-	
-	/*@Bean
-	Saml2LogoutRequestResolver logoutRequestResolver(RelyingPartyRegistrationResolver registrationResolver) {
-		OpenSaml4LogoutRequestResolver logoutRequestResolver=new OpenSaml4LogoutRequestResolver(registrationResolver);
-		logoutRequestResolver.setParametersConsumer((parameters) -> {
-			String name = ((Saml2AuthenticatedPrincipal) parameters.getAuthentication().getPrincipal()).getFirstAttribute("CustomAttribute");
-			String format = "urn:oasis:names:tc:SAML:2.0:nameid-format:transient";
-			LogoutRequest logoutRequest = parameters.getLogoutRequest();
-			NameID nameId = logoutRequest.getNameID();
-			nameId.setValue(name);
-			nameId.setFormat(format);
-		});
-		return logoutRequestResolver;
-	}*/
+
 
 	
 	
