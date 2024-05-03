d22b373118a968e7b95624a1b6686938ff3885f4
Ruben Marin <ams.rmarin@ext.aragon.es>
2024-05-03 12:21:52 +0200

    cambio logout url 14

--- src/main/java/es/aragon/example/springboot/saml2/SecurityConfig.java
+++ src/main/java/es/aragon/example/springboot/saml2/SecurityConfig.java
@@ -49,12 +49,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	    		authorize.antMatchers("/","/logout").permitAll().anyRequest().authenticated()
 	        )
 	    	.saml2Login(Customizer.withDefaults())
-	    	.saml2Logout(Customizer.withDefaults())
+	    	//.saml2Logout(Customizer.withDefaults())
 	    	.logout((logout) ->
 				logout.deleteCookies("remove")
 					.invalidateHttpSession(true)
 					.clearAuthentication(true)
-					.logoutUrl("/mylogout")
+					.logoutUrl("/logout")
 					.logoutSuccessUrl("/"));
 
 		// add auto-generation of ServiceProvider Metadata
