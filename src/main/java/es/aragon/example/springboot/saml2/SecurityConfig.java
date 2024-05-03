cfc1cf9b6a4fcf65824d4af2e2b8d24b7e2ab450
Ruben Marin <ams.rmarin@ext.aragon.es>
2024-05-03 13:49:26 +0200

    cambio logout good

--- src/main/java/es/aragon/example/springboot/saml2/SecurityConfig.java
+++ src/main/java/es/aragon/example/springboot/saml2/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	        )
 	    	.saml2Login(Customizer.withDefaults())
 	    	.saml2Logout(Customizer.withDefaults())
-	    	//.logout((logout) ->
+	    	.logout((logout) ->
 				logout.deleteCookies("remove")
 					.invalidateHttpSession(true)
 					.clearAuthentication(true)
@@ -92,7 +92,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	            .assertingPartyDetails(party -> party
 	                .entityId("http://ssoa.aragon.es/adfs/services/trust")
 	                .singleSignOnServiceLocation("https://ssoa.aragon.es/adfs/ls/IdpInitiatedSignon")
-	                .singleLogoutServiceLocation("https://samladfs-tpvams.apps.pre.aragon.es/logout/saml2/slo")
+	                //.singleLogoutServiceLocation("https://samladfs-tpvams.apps.pre.aragon.es/logout/saml2/slo")
+	                .singleLogoutServiceLocation("https://ssoa.aragon.es/adfs/oauth2/logout")
 	                //.singleLogoutServiceLocation("https://ssoa.aragon.es/adfs/ls/?wa=wsignout1.0")
 	                .wantAuthnRequestsSigned(false)
 	                .verificationX509Credentials(c -> c.add(credential))
